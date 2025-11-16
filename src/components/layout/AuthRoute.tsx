5d6e2567a1bee63523fc4a3183a845adc1c4d644
Geskha <prasetyarestu123@gmail.com>
2025-11-17 02:05:38 +0700

    feat: protect auth route

---
+++ src/components/layout/AuthRoute.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { PropsWithChildren, useEffect } from "react";
+import { supabase } from "~/lib/supabase/client";
+
+export const AuthRoute = (props: PropsWithChildren) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    void (async function () {
+      const { data, error } = await supabase.auth.getUser();
+
+      if (!data.user) {
+        await router.replace("/");
+      }
+    })();
+  }, []);
+
+  return props.children;
+};
--- src/features/profile/pages/ProfilePage.tsx
+++ src/features/profile/pages/ProfilePage.tsx
@@ -22,6 +22,7 @@ import { EditProfileFormSchema } from "../forms/edit-profile";
 import { zodResolver } from "@hookform/resolvers/zod";
 import EditProfileFormInner from "../components/EditProfileFormInner";
 import { EditProfileFormSkeleton } from "../components/EditProfileFormSkeleton";
+import { AuthRoute } from "../../../components/layout/AuthRoute";
 
 export const ProfilePage = () => {
   const { data: getProfileData } = api.profile.getProfile.useQuery();
@@ -33,47 +34,49 @@ export const ProfilePage = () => {
   };
 
   return (
-    <PageContainer>
-      <SectionContainer padded minFullscreen className="gap-y-6 py-8">
-        <h1 className="text-3xl font-semibold">Profile Setting</h1>
-        <Card>
-          <CardContent className="flex gap-6 pt-6">
-            <div className="flex flex-col gap-2">
-              <Avatar className="size-24">
-                <AvatarFallback>VF</AvatarFallback>
-                <AvatarImage />
-              </Avatar>
-              <div className="relative">
-                <Button
-                  className="absolute bottom-0 right-0 h-8 w-8"
-                  onClick={handleOpenFileExplorer}
-                >
-                  <Camera />
-                </Button>
+    <AuthRoute>
+      <PageContainer>
+        <SectionContainer padded minFullscreen className="gap-y-6 py-8">
+          <h1 className="text-3xl font-semibold">Profile Setting</h1>
+          <Card>
+            <CardContent className="flex gap-6 pt-6">
+              <div className="flex flex-col gap-2">
+                <Avatar className="size-24">
+                  <AvatarFallback>VF</AvatarFallback>
+                  <AvatarImage />
+                </Avatar>
+                <div className="relative">
+                  <Button
+                    className="absolute bottom-0 right-0 h-8 w-8"
+                    onClick={handleOpenFileExplorer}
+                  >
+                    <Camera />
+                  </Button>
+                </div>
+                <input type="file" ref={inputFileRef} className="hidden" />
               </div>
-              <input type="file" ref={inputFileRef} className="hidden" />
-            </div>
 
-            <div className="grid flex-1 grid-cols-2 gap-y-4">
-              {getProfileData ? (
-                <EditProfileFormInner
-                  defaultValues={{
-                    bio: getProfileData?.bio,
-                    username: getProfileData?.username,
-                  }}
-                />
-              ) : (
-                <EditProfileFormSkeleton />
-              )}
-            </div>
-          </CardContent>
-        </Card>
+              <div className="grid flex-1 grid-cols-2 gap-y-4">
+                {getProfileData ? (
+                  <EditProfileFormInner
+                    defaultValues={{
+                      bio: getProfileData?.bio,
+                      username: getProfileData?.username,
+                    }}
+                  />
+                ) : (
+                  <EditProfileFormSkeleton />
+                )}
+              </div>
+            </CardContent>
+          </Card>
 
-        <div className="flex w-full justify-end">
-          <Button>Simpan</Button>
-        </div>
-      </SectionContainer>
-    </PageContainer>
+          <div className="flex w-full justify-end">
+            <Button>Simpan</Button>
+          </div>
+        </SectionContainer>
+      </PageContainer>
+    </AuthRoute>
   );
 };
 
